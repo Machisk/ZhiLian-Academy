f628033cc682fff17818e5324ea92eb5dc564918
fanzixuan <fanzixuan@xiaomi.com>
2025-03-20 10:41:02 +0800

    feat: project init

---
+++ front-end/Zhilian-web/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from './components/ui/button'
+
+
+function App() {
+  return (
+    <>
+      <Button>123</Button>
+      <h1>Vite + React</h1>
+    </>
+  )
+}
+
+export default App
